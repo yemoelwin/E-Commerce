a0f59d135312c9ad8c47a2f39b29aef663c25195
Mona Lisa <yemoelwin142@gmail.com>
2023-07-21 15:00:41 +0630

    last update

--- Front-end/src/redux/reducers/index.js
+++ Front-end/src/redux/reducers/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const index = () => {
-    return (
-        <div>index</div>
-    )
-}
-
-export default index
+import { combineReducers } from "redux";
+// import { productsReducer, selectedProductsReducer } from "./productsReducer";
+// import { userReducer } from "./userReducer";
+const reducers = combineReducers({
+//   allProducts: productsReducer,
+//   product: selectedProductsReducer,
+//   user: userReducer
+});
+export default reducers;
--- Front-end/src/redux/store.js
+++ Front-end/src/redux/store.js
@@ -1,12 +1,16 @@
-import { configureStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers/index";
 
-import thunk from 'redux-thunk';
-import reducers from './reducers/index'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = configureStore(
-    reducers,
-    composeEnhancers(applyMiddleware(thunk))
+
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk))
+  // {},
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
